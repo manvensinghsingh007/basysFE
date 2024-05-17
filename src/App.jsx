0c18adccc322210658f5da885c4fcdccfac88217
Manvendra Singh <manvensingh07@gmail.com>
2024-05-17 08:40:44 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("");
+  const [valueUpdated, setValueUpdated] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    if (value != "" && valueUpdated == true) {
+      setLoading(true);
+      let res = await fetch(
+        `https://jsonmock.hackerrank.com/api/stocks?date=${value}`
+      );
+      let resJson = await res.json();
+
+      if (resJson.data[0] == undefined) {
+        resJson.data[0] = [];
+      }
+
+      setData(resJson.data[0]);
+      setValueUpdated(false);
+      setLoading(false);
+    }
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="appContainer">
+      <div className="searchSection">
+        <input
+          type="text"
+          onChange={(e) => {
+            setValueUpdated(true);
+            setValue(e.target.value);
+          }}
+          placeholder="5-January-2000"
+          onKeyDown={(k) => {
+            if (k.key == "Enter") fetchData();
+          }}
+          data-testid="app-input"
+        />
+        <button onClick={() => fetchData()} data-testid="submit-button">
+          Search
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="dataSection">
+        {loading == true ? (
+          <div>Loading..</div>
+        ) : data == null ? (
+          <div></div>
+        ) : (
+          <div>
+            {data.length == 0 ? (
+              <div data-testid="no-result">No Results Found</div>
+            ) : (
+              <div>
+                <ul data-testid="stock-data">
+                  <li>Open: {data.open}</li>
+                  <li>Close: {data.close}</li>
+                  <li>High: {data.high}</li>
+                  <li>Low: {data.low}</li>
+                </ul>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
